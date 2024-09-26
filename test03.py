146782a2150e5598dbfc65c5ba95eeaf414c7d48
user02 <user02@abc.com>
2024-09-26 11:16:18 +0900

    modify test03.py by user02

--- test03.py
+++ test03.py
@@ -1,9 +1,8 @@
-#test02.py
+#test03.py
 
-def mulNum(a,b):
-    result=a*b
-    retrun result
-    
-num01 = 10
-num02 = 20
-print(mulNum(num01,num02))
+def sayHello(name):
+    return 'Hi, ' + name #user02 수정
+inName =Tom
+print(sayHello(intName))
+
+print('modified by user02') #User02수정
