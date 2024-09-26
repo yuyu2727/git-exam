8391faef29337f50fad76c39da79fd11878388e7
unknown <yu@naver.com>
2024-09-26 09:39:57 +0900

    first commit

---
+++ test01.py
@@ -0,0 +1,9 @@
+#test01.py
+
+def addNum(a,b):
+    sum= a+b
+    retrun sum
+    
+num01 = 10
+num02 = 20
+print(addNum(num01,num02))
---
+++ test03.py
@@ -0,0 +1,9 @@
+#test02.py
+
+def mulNum(a,b):
+    result=a*b
+    retrun result
+    
+num01 = 10
+num02 = 20
+print(mulNum(num01,num02))
